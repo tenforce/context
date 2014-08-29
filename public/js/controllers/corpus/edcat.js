e5f46c292a624e6c0c95b744343f5aa3a8276450
Aad Versteden <madnificent@gmail.com>
2014-08-29 21:37:02 +0200

    Returing datasets for the marked entities in the article

--- public/js/controllers/corpus/edcat.js
+++ public/js/controllers/corpus/edcat.js
@@ -1,24 +1,77 @@
 // includes
 var FacetsProcesser = require('../../modules/FacetsProcesser');
 
-//renders the d3 graph
+distillEnglish = function(title) {
+    if( !title )
+        return ""
+    for( var i = 0 ; i < title.length ; i++ ) {
+        if( (typeof title[i] != "string")
+            && title[i]["en"] ){
+            return title[i]["en"];
+        }
+    }
+    for( var i = 0 ; i < title.length ; i++ ) {
+        if( typeof title[i] == "string" )
+            return title[i];
+    }
+    return title;
+}
+
 module.exports = function EdcatDatasetsController($scope, $state, $sce) {
-    // get data
-    console.log('scope: ' + $scope);
-    console.log('state: ' + $state);
-    console.log('sce: ' + $sce);
 
     // get data
     $.getJSON('/api/corpus/' + $scope.currentCorpus._id + '/facets', function(data) {
-        facetsData = FacetsProcesser.processData(data);
-        // types
-        types = facetsData.types;
-        console.log('types: ' + types);
-        // entities
-        entities = facetsData.entities;
-        console.log('entities: ' + entities);
-        // articles
-        articles = facetsData.articles;
-        console.log('articles: ' + articles);
+        allEntities = data.articles[0].entities;
+        foundUris = []
+        foundEntities = []
+        for( var i = 0 ; i < allEntities.length ; i++ ) {
+            if( $.inArray(allEntities[i].uri, foundUris) === -1 ) {
+                foundEntities.push(allEntities[i]);
+                foundUris.push(allEntities[i].uri)
+            }
+        }
+
+        $scope.entities = foundEntities;
+        
+
+        $scope.entities.map( function( entity ) {
+            entity.active = Math.random() > 0.5 ? true : false;
+        } );
+
+        $scope.toggleActive = function( item ) {
+            item.active = !item.active;
+            $scope.fetchDatasets();
+        }
+
+        $scope.itemActive = function( item ) {
+            return item.active;
+        }
+
+        $scope.fetchDatasets = function() {
+            activeEntities = []
+            $scope.datasets = []
+            $scope.entities.map( function( entity ) {
+                if( entity.active )
+                    activeEntities.push( entity );
+            });
+
+            // edcat/context/search?tagIds[]=http://dbpedia.org/resource/Communes_of_Switzerland
+            // path = "http://localhost:8081/edcat/contexts/search?" + activeEntities.map(function(entity){ return "tagIds[]=\"" + encodeURIComponent(entity.uri) + "\""; }).join("&")
+            path = "http://localhost:8081/edcat/context/search?" + activeEntities.map(function(entity){ return "tagIds[]=" + encodeURIComponent(entity.uri) + ""; }).join("&")
+            
+            console.log("Requesting " + path);
+
+            $.getJSON(path, function(data) {
+                $scope.datasets = data;
+                for( var i = 0 ; i < data.length ; i++ ){
+                    data[i].title = distillEnglish(data[i].title);
+                    data[i].description = distillEnglish(data[i].description);
+                }
+                $state.reload();
+            });
+        };
+
+        $scope.fetchDatasets();
+
     });
 };
